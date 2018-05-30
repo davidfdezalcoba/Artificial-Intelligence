beed81e9c85528dd6fc7d2bdea569f912d332841
david <davidfdezalcoba@gmail.com>
2018-05-31 01:07:11 +0200

    Minor fixes

--- task3/task3/CandidateElim/Enums/Material.java
+++
@@ -1,5 +0,0 @@
-package task3.CandidateElim.Enums;
-
-public enum Material {
-	Wood, Metal, Paper
-}
--- task3/task3/CandidateElim/Enums/Shape.java
+++
@@ -1,5 +0,0 @@
-package task3.CandidateElim.Enums;
-
-public enum Shape {
-	Triangle, Rectangle, Circle
-}
